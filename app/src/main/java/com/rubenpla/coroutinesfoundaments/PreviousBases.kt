af0b174cee684650afd39b6513ce525dba0f8c80
Ruben Pla Ferrero <rubenpla.develop@gmail.com>
2023-06-21 15:50:00 +0200

    feat: First coroutines basics

--- app/src/main/java/com/rubenpla/coroutinesfoundaments/PreviousBases.kt
+++ app/src/main/java/com/rubenpla/coroutinesfoundaments/PreviousBases.kt
@@ -1,3 +1,20 @@
 package com.rubenpla.coroutinesfoundaments
 
+fun main() {
+    lambda()
+}
 
+fun lambda() {
+    println(multi(22, 3))
+    println(multiLambda(22, 3) { result ->
+        println("Lambda result: $result")
+    })
+}
+
+fun multiLambda(x: Int, y: Int, callback : (result : Int) -> Unit) {
+    callback(x * y)
+}
+
+fun multi(x: Int, y: Int): Int {
+    return x * y
+}
