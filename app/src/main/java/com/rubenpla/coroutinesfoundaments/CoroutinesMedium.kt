a250954b2a7874d3b6721537848484487d8a7c9f
Ruben Pla Ferrero <rubenpla.develop@gmail.com>
2023-07-03 15:57:01 +0200

    feat: CoroutinesMedium #2
    - Nested Coroutines
    - Section 4/29

--- app/src/main/java/com/rubenpla/coroutinesfoundaments/CoroutinesMedium.kt
+++ app/src/main/java/com/rubenpla/coroutinesfoundaments/CoroutinesMedium.kt
@@ -1,14 +1,66 @@
 package com.rubenpla.coroutinesfoundaments
 
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.newSingleThreadContext
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 
 fun main() {
-    dispatchers()
+    //dispatchers()
+    nestedCoroutines()
 }
 
+@OptIn(DelicateCoroutinesApi::class)
+fun nestedCoroutines() {
+    runBlocking {
+        newTopic("Nested Coroutines")
+
+        val job = launch {
+            startMsg()
+
+            launch(newSingleThreadContext("Any task thread #1")) {
+                startMsg()
+                delay(someTime())
+                println("   - Any task thread #1")
+                endMsg()
+            }
+
+            launch(Dispatchers.IO) {
+                startMsg()
+
+                launch(newSingleThreadContext("Any task sub-thread #2")) {
+                        startMsg()
+                        delay(someTime())
+                        println("   - Any task sub-thread #2")
+                        endMsg()
+                }
+
+                delay(someTime())
+                println("   - Any task thread #2")
+                endMsg()
+            }
+
+            var sum = 0
+            (1..100).forEach {
+                sum+=it
+                delay(someTime()/100)
+            }
+
+            println("Sum = $sum")
+            endMsg()
+        }
+
+        delay(someTime()/2)
+        job.cancel()
+        println("   ----- Job Cancelled")
+    }
+}
+
+@OptIn(DelicateCoroutinesApi::class)
 fun dispatchers() {
     runBlocking {
         newTopic("Dispatchers")
